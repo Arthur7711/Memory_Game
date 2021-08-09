bce7037d33fe9c53b55849e47d938f7d7e1b24ca
Arthur Ghazaryan <newartroll11@gmail.com>
2021-08-09 21:42:41 +0400

    add divs

--- src/memoryGame/MemoryGame.js
+++ src/memoryGame/MemoryGame.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import s from "./MemoryGame.module.css";
+import flag from "./imgs/Armenia.jpg";
+import Aragacotn from "./imgs/Aragacotn.jpg";
+import Ararat from "./imgs/Ararat.jpg";
+import Armavir from "./imgs/Armavir.png";
+import Gexarquniq from "./imgs/Gexarquniq.jpg";
+import Kotayk from "./imgs/Kotayk.jpg";
+import Lori from "./imgs/Lori.jpg";
+import Shirak from "./imgs/Shirak.jpg";
+import Syunik from "./imgs/Syunik.jpg";
+import Tavush from "./imgs/Tavush.jpg";
+import VayocDzor from "./imgs/VayocDzor.png";
 
 export default function MemoryGame() {
-    return (
-        <div>
-            memoryGame
+  const region = [
+    Aragacotn,
+    Ararat,
+    Armavir,
+    Gexarquniq,
+    Kotayk,
+    Lori,
+    Shirak,
+    Syunik,
+    Tavush,
+    VayocDzor,
+  ];
+
+  return (
+    <div className={s.container}>
+      {region.map((item) => (
+        <div className={s.card}>
+          <div className={s.details}>
+            <img alt="img" src={flag} />
+          </div>
+          <div className={s.imgBox}>
+            <img alt="img" src={item} />
+          </div>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
